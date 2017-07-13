788de8862ce26ccda6b0a89dbc3375e8950e9b00
Sooraj Narayan <proflicxx@gmail.com>
2017-07-13 10:51:37 +0530

    --commiting generator changes

--- generate.js
+++ generate.js
@@ -1,9 +1,11 @@
 var stdin = process.openStdin();
 var fs = require('fs');
-console.log("Enter the name of the component :")
+console.log('\x1b[33m%s\x1b[0m', '1. Generate page component');
+console.log('\x1b[33m%s\x1b[0m', '2. Delete page component');
+console.log('\x1b[32m%s\x1b[0m', '\nEnter the number of action to perform >');
 stdin.addListener("data", function (d) {
 	var cName = d.toString().trim();
-	var viewsPath = "src/views/" + cName;
+	var viewsPath = "src/pages/" + cName;
 	var routerPath = "router/"
 	var getDirName = require('path').dirname;
 	var viewcontents = "<template>\n \n  </template> \n \n <script src='./" + cName + "controller.js'></script>";
@@ -24,7 +26,7 @@ stdin.addListener("data", function (d) {
 				console.log(cName + ".vue file created");
 			}
 		});
-		fs.writeFile(viewsPath + "/" + cName + "controller.js", controllerContents, function (err) {
+		fs.writeFile(viewsPath + "/" + cName + ".controller.js", controllerContents, function (err) {
 			if (err) {
 				console.log(err);
 				process.exit();
@@ -32,7 +34,7 @@ stdin.addListener("data", function (d) {
 				console.log(cName + ".js controller created");
 			}
 		});
-		fs.writeFile(viewsPath + "/" + cName + "service.js", serviceContents, function (err) {
+		fs.writeFile(viewsPath + "/" + cName + ".service.js", serviceContents, function (err) {
 			if (err) {
 				console.log(err);
 				process.exit();
@@ -40,15 +42,15 @@ stdin.addListener("data", function (d) {
 				console.log(cName + ".js service created");
 			}
 		});
-		fs.readFile("router/index.js", 'utf8', function (err, data) {
+		fs.readFile("src/router/index.js", 'utf8', function (err, data) {
 			if (err) {
 				process.exit();
 			}
-			var imp = "import " + cName + "  from @/views/" + cName + "/" + cName + "\n //ImportModules";
+			var imp = "import " + cName + "  from '@/pages/" + cName + "/" + cName + "' \n //ImportModules";
 			var rNot = "{ \n path: '/" + cName + "',\n name:'" + cName + "',\n component:'" + cName + "'\n},\n //routes";
 			var result = data.replace(/\/\/ImportModules/g, imp);
 			result = result.replace(/\/\/routes/g, rNot);
-			fs.writeFile("router/index.js", result, 'utf8', function (err) {
+			fs.writeFile("src/router/index.js", result, 'utf8', function (err) {
 				if (err) {
 					console.log(err);
 					process.exit();
